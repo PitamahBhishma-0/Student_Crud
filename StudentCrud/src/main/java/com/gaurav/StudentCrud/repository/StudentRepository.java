76817fc6c6729dc8882057bf899f6c8a5d7cf970
गौरव बस्याल <gauravbasyal789@gmail.com>
2022-04-02 17:14:52 +0545

    Student_crud

---
+++ StudentCrud/src/main/java/com/gaurav/StudentCrud/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.gaurav.StudentCrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gaurav.StudentCrud.domain.Student;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+ 
+}
